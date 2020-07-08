80b84f016f0ae2d4089e0f50f4145a85cc46f818
UnderKo <hhit934@gmail.com>
2020-07-08 23:14:44 +0300

    Добавьте файлы проекта.

---
+++ NewForm/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+
+
+namespace NewForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                Color Cscreent = lib.ScreenLib.GetColorPixel(Cursor.Position);
+                this.BackColor = Cscreent;
+                ColorLabel.Text = Cscreent.ToString();
+            }  
+        }
+    }
+}
---
+++ NewForm/lib/ScreenLib.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewForm.lib
+{
+    static class ScreenLib
+    {
+        public static int CountScreen()
+        {
+            return Screen.AllScreens.Length;
+        }
+        public static Size GetScreenSize()
+        {
+            return Screen.PrimaryScreen.Bounds.Size;
+        }
+        public static Color GetColorPixel( Point P )
+        {
+            Bitmap Bitmap_ = new Bitmap(1, 1);
+            Rectangle Rectangle_ = new Rectangle(P.X, P.Y, 1, 1);
+            Graphics g = Graphics.FromImage(Bitmap_);
+            g.CopyFromScreen(Rectangle_.Location, Point.Empty, Rectangle_.Size);
+            return Bitmap_.GetPixel(0, 0);
+        }
+
+
+        public static bool isEqualColor( this Color C , Color C2)
+        {
+
+            return
+                C.R == C2.R &&
+                C.G == C2.G &&
+                C.B == C2.B ;
+        }
+
+
+
+    }
+
+
+}
